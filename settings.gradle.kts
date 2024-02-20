900a29900e4cd2378bcad73b31cd38b50eaf053e
Brayan Bermudez <bfbermudezc@gmail.com>
2024-02-20 16:02:05 -0500

    initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Yu_Gi_Oh_app"
+include(":app")
+ 
